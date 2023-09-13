604e6440e7809c00c58816aab1837a5c40db96d3
Federico Capizzi <federico.capiz02@gmail.com>
2023-09-13 16:55:23 +0200

    versione 'finale'

--- logica.js
+++ logica.js
@@ -1,11 +1,14 @@
-const newcard = document.getElementById('partefissa')
+const newcard = document.getElementById('partefissa');
 newcard.addEventListener ('submit',function(e){
-    e.preventDefault()
-    
-    const newContactCard = document.createElement('div'); 
-    newContactCard.classList.add('list-card');
+    e.preventDefault();
+    const newinput = document.getElementById('text');
+    const task = newinput.value;
+    console.log(task);
 
-    newContactCard.innerHTML = document.createElement('div');
-    <p>${newcard}</p>
-
+    const newContactCard = document.createElement('div');
+    newContactCard.classList.add('topolino') 
+    const newtext = document.getElementById('saved-text');
+    newContactCard.innerHTML = `<p class='marcoo'> You should do ${text.value} </p><button class='marco' ></button><button class='marco' ></button> `;
+    console.log(newtext)
+    newtext.appendChild(newContactCard);
 })
